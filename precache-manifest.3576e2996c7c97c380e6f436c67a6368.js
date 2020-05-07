4a228b37f42df763add930a10f4ab0010c9ea2b0
Kemi Thomas <kemothom71@gmail.com>
2020-05-06 22:29:43 -0400

    Updates

---
+++ precache-manifest.3576e2996c7c97c380e6f436c67a6368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a7315e532beecfc4683c44d3f12031",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "33bcb075949d6e6df19b",
+    "url": "/movie-search-app/static/css/main.147443ec.chunk.css"
+  },
+  {
+    "revision": "92414624247123f88e54",
+    "url": "/movie-search-app/static/js/2.2d58c3be.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/movie-search-app/static/js/2.2d58c3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33bcb075949d6e6df19b",
+    "url": "/movie-search-app/static/js/main.538ddbf5.chunk.js"
+  },
+  {
+    "revision": "1f23b7526eaf03ade22b",
+    "url": "/movie-search-app/static/js/runtime-main.ac1c0787.js"
+  }
+]);
